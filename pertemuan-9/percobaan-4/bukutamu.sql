ec6d22297666923895d732ee448830d0e612c5f1
Muhammad Rizki Oktarlis Setia Budi <mrizki.14115032@student.itera.ac.id>
2019-10-31 11:21:16 +0700

    input table buku tamu

---
+++ pertemuan-9/percobaan-4/bukutamu.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 31 Okt 2019 pada 04.12
+-- Versi server: 10.4.8-MariaDB
+-- Versi PHP: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `negara`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `bukutamu`
+--
+
+CREATE TABLE `bukutamu` (
+  `nama` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `komentar` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
